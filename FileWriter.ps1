5b0539a5f3f44a16e1f36806bbcc24761affbf5d
Christopher Roelle <christopher.roelle@gmail.com>
2023-05-03 15:43:58 -0500

    FileWriter.ps1
    Adding FileWrite.ps1
    
    Has base features for Writing to a file, Deleting an existing file, and clearing an existing file's contents.

---
+++ FileWriter.ps1
@@ -0,0 +1,84 @@
+﻿<#######################################################################################>
+<# Author : Christopher Roelle
+<# Date   : 05-03-2023
+<# Purpose: Link the file for writing to files functionality.
+<#          Link the file using . 'path\FileWriter.ps1'
+<#######################################################################################>
+
+#We are linked.
+Write-Output('Linked FileWriter.ps1.')
+
+#Writes line to file. If file doesn't exist, adds the file.
+function WriteToFile{
+
+    param(
+        $Path,
+        $Filename,
+        $Line
+    )
+
+    Write-Output("Checking if file exists...")
+    if(!(Test-Path($Path + $Filename))){
+        Write-Output('')
+        Write-Output("File doesn't exist, writing file.")
+        New-Item -Path $Path -Name $Filename
+        Write-Output('')
+    }
+    Write-Output("File exists!")
+
+    $fullName = $Path + '\' + $Filename
+
+    Add-Content -Path $fullName -Value $Line
+}
+
+#Useful for deleting the existing file.
+function DeleteExistingFile{
+
+    param(
+        $Path,
+        $Filename
+    )
+
+    Write-Output("Checking if file exists...")
+
+    if(!(Test-Path($Path + $Filename))){
+        Write-Output('')
+        Write-Output("File doesn't exist, skipping.")
+        Write-Output('')
+    }
+    else{
+        Write-Output("File exists. Deleting...")
+
+        $fullName = $Path + '\' + $Filename
+
+        Remove-Item -Path $fullName
+
+        Write-Output($fullname + " was deleted.")
+    }
+
+}
+
+#Clears the contents of the file. Sets contents to blank.
+function ClearFileContents{
+    param(
+        $Path,
+        $Filename
+    )
+
+    Write-Output("Checking if file exists...")
+
+    if(!(Test-Path($Path + $Filename))){
+        Write-Output('')
+        Write-Output("File doesn't exist, skipping.")
+        Write-Output('')
+    }
+    else{
+        Write-Output("File exists. Clearing contents...")
+
+        $fullName = $Path + '\' + $Filename
+
+        Set-Content -Path $fullName -Value ""
+
+        Write-Output($fullname + " contents cleared.")
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Powerhsell Tools
  Repository of useful helper scripts/modules for Powershell.
+
+ Contains useful helper scripts that you can link into your main ps1 script file.
+
+ Download the ps1 file, then link to it using:
+
+ . 'path\filename.ps1'
